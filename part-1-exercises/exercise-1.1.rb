07fb29218ed72c13692348d514ab4e7b678b3749
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2022-02-21 19:43:26 -0600

    Create exercise-1.1.rb

---
+++ part-1-exercises/exercise-1.1.rb
@@ -0,0 +1,8 @@
+# p "Hello, World!"
+# puts "Hello, World!"
+# print "Hello, World!"
+my_name = "Paco"
+# print my_name
+beginning_sentance = "My name is "
+full_sentance = beginning_sentance + my_name
+print full_sentance
