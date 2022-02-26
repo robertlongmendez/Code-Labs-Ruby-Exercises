2918f990c791e2f30b6d49e18ebe74a0b4c138bd
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2022-02-26 12:57:39 -0600

    favorites list update

--- part-1-exercises/exercise-1.3.rb
+++ part-1-exercises/exercise-1.3.rb
@@ -1,20 +1,24 @@
 
 def favorite_movie(movie)
-    movie = "Attack of the killer tomatoes!"
-    print "My favorite movie is #{movie}"
+    print "My favorite movie is: #{movie}"
 #    favorite_movie = print "My favorite movie is #{movie}"
     # TODO: use string interpolation to print "My favorite movie is " and include the movie parameter.
 end
 
 def favorite_food(food)
+    print "My favorite food is: #{food}"
     # TODO: use string interpolation to print "My favorite food is " and include the food parameter.
 end
 
 def favorite_drink(drink)
+    print "My favorite drink is: #{drink}"
     # TODO: use string interpolation to print "My favorite drink is " and include the drink parameter.
 end 
 
 def list_of_favorite_things(movie, food, drink)
+    favorite_movie(movie)  
+    favorite_food(food)
+    favorite_drink(drink)
     # TODO: Call favorite_movie, pass in the parameter movie as an argument
     # TODO: Call favorite_food, pass in the parameter food as an argument 
     # TODO: Call favorite_drink, pass in the parameter drink as an argument 
@@ -22,3 +26,13 @@ def list_of_favorite_things(movie, food, drink)
 end
 
 # TODO: Call list_of_favorite_things, include three arguments
+puts "Your Favorite Everything List!"
+20.times { print ";p " }
+puts
+puts "Please enter your favorite movie"
+movie = gets.chomp
+puts "Please enter your favorite food"
+food = gets.chomp
+puts "Please enter your favorite drink"
+drink = gets.chomp
+list_of_favorite_things
